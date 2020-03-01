76af44e9a37a5642ecc3dd8da26b01b96518ab2d
janonoly <1369021134@qq.com>
2020-03-01 16:22:36 +0800

    models初步完成

--- config/models.py
+++ config/models.py
@@ -16,7 +16,7 @@ class Link(models.Model):
     weight = models.PositiveIntegerField(default=1, choices=zip(range(1, 6),
                                                                 range(1, 6)),verbose_name="权重",
                                          help_text="权重高展示顺序靠前")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -43,7 +43,7 @@ class SideBar(models.Model):
                                help_text="如果设置的不是HTML类型，可为空")
     status = models.PositiveSmallIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS,
                                               verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
