200efa391052ee46be39f81f73bca9bfd49c94ae
janonoly <1369021134@qq.com>
2020-03-02 17:00:34 +0800

    7.4.1章节完成

--- typeidea/urls.py
+++ typeidea/urls.py
@@ -21,10 +21,10 @@ from config.views import links
 from typeidea.custom_site import custom_site
 
 urlpatterns = [
-    re_path(r'^$', post_list),
-    re_path(r'^category/(?P<category_id>\d+)/$', post_list),
-    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    re_path(r'^post/(?P<post_id>\d+).html$', post_detail),
+    re_path(r'^$', post_list, name='index'),
+    re_path(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    re_path(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
     re_path(r'^links/$',links),
     path('super_admin/', admin.site.urls),
     path('admin/', custom_site.urls),
